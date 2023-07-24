e66f7f3d0e5d6fa0ec750829fef752cd9bd7cbe1
Rayfire2021 <93227458+Rayfire2021@users.noreply.github.com>
2023-07-24 17:40:01 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload() 
 {
-
+moustache = loadImage('https://i.postimg.cc/QdtMNxxz/moustacheimg.png');
 }
 
 
@@ -26,6 +29,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
@@ -34,6 +39,7 @@ function gotPoses(results)
 function draw() 
 {
     image(video, 0, 0, 300, 300);
+    image(moustache, noseX-30, noseY-15, 75, 75);
 }
 
 function take_snapshot()
